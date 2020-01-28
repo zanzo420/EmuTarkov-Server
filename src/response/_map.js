e6a2d50a5c1a5a2f164ff70d1fc7fc59991d0102
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-28 10:38:01 +0100

    Replace backendurl in map

--- src/response/_map.js
+++ src/response/_map.js
@@ -11,10 +11,12 @@ function get(mapName) {
         map = presetNames[utility.getRandomInt(0, presetNames.length - 1)];
     } else {
         map = presetNames[settings.gameplay.location.forceMapId];
-    }    
+    }
 
-    let data = json.read(mapNode[map]);
     logger.logWarning("[MAP." + mapName + "]: " + map);
+        
+    let data = json.read(mapNode[map]);
+    data.BackendUrl = "https://' + ip +'/";
     return data;
 }
 
