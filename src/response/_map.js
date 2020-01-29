f1fcd5769bb600b492186b37a7bf00020f7cad6a
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-29 20:25:37 +0100

    Map loot generation Works(TM)

--- src/response/_map.js
+++ src/response/_map.js
@@ -5,20 +5,24 @@ require("../libs.js");
 function get(mapName) {        
     let data = json.parse(json.read(filepaths.maps[mapName].base));
     let lootCount = settings.gameplay.maploot[mapName];
-    let lootFiles = Object.keys(filepaths.maps[mapName].loot);
+    let keys = Object.keys(filepaths.maps[mapName].loot);
 
     // set backend url
     data.BackendUrl = "https://' + ip +'/";
-    
+
+    console.log(filepaths.maps[mapName].loot);
+
     // generate loot
     if (mapName !== "hideout") {
-        for (let loot in lootCount) {
-            data.Loot.push(json.parse(json.read(filepaths.maps[mapName].loot[lootFiles[loot]])));
+        for (let i = 0; i < lootCount; i++) {
+            let item = filepaths.maps[mapName].loot[keys[utility.getRandomInt(0, keys.length - 1)]];
+            console.log(item);
+            data.Loot.push(json.parse(json.read(item)));
         }
     }
-    
+
     // TODO: code here
-    console.log(data);
+    console.log(data.Loot);
     return json.stringify(data);
 }
 
