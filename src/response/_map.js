3074f66ee08a8371d34dc6b9fe3583aa3680bc85
Merijn Hendriks <innohurrytocode@gmail.com>
2020-01-29 19:49:23 +0100

    [WIP] implement maploot logic

--- src/response/_map.js
+++ src/response/_map.js
@@ -4,13 +4,21 @@ require("../libs.js");
 
 function get(mapName) {        
     let data = json.parse(json.read(filepaths.maps[mapName].base));
+    let lootCount = settings.gameplay.maploot[mapName];
+    let lootFiles = Object.keys(filepaths.maps[mapName].loot);
 
     // set backend url
     data.BackendUrl = "https://' + ip +'/";
     
     // generate loot
+    if (mapName !== "hideout") {
+        for (let loot in lootCount) {
+            data.Loot.push(json.parse(json.read(filepaths.maps[mapName].loot[lootFiles[loot]])));
+        }
+    }
+    
     // TODO: code here
-
+    console.log(data);
     return json.stringify(data);
 }
 
