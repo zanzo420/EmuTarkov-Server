7fdab43de7a925ee170a1f2b2e10c517661ce84f
Merijn Hendriks <innohurrytocode@gmail.com>
2020-02-02 02:27:35 +0100

    Health _should_ work now

--- src/response/_health.js
+++ src/response/_health.js
@@ -31,41 +31,34 @@ class HealthServer {
         }
 
         // update health to apply after raid
-        let health = this.healths[sessionID];
-    
+        let node = this.healths[sessionID];
+
         switch (info.type) {
             case "HydrationChanged":
-                health["Hydration"] += parseInt(info.diff);
-                break;
-    
             case "EnergyChanged":
-                health["Energy"] += parseInt(info.diff);
+                node[(info.type).replace("Changed", "")] += parseInt(info.diff);
                 break;
     
             case "HealthChanged":
-                switch(info.bodyPart) {
-                    case "Head": healths[sessionID].Head = parseInt(info.value); break;
-                    case "Chest": healths[sessionID].Chest = parseInt(info.value); break;
-                    case "Stomach": healths[sessionID].Stomach = parseInt(info.value); break;
-                    case "LeftArm": healths[sessionID].LeftArm = parseInt(info.value); break;
-                    case "RightArm": healths[sessionID].RightArm = parseInt(info.value); break;
-                    case "LeftLeg": healths[sessionID].LeftLeg = parseInt(info.value); break;
-                    case "RightLeg": healths[sessionID].RightLeg = parseInt(info.value); break;
-                }
+                node[info.bodyPart] = info.value;
                 break;
     
             case "Died":
-                healths[sessionID].Head = 0;
-                healths[sessionID].Chest = 0;
-                healths[sessionID].Stomach = 0;
-                healths[sessionID].LeftArm = 0;
-                healths[sessionID].RightArm = 0;
-                healths[sessionID].LeftLeg = 0;
-                healths[sessionID].RightLeg = 0;
+                node = {
+                    "Hydration": this.healths[sessionID].Hydration,
+                    "Energy": this.healths[sessionID].Energy,
+                    "Head": 0,
+                    "Chest": 0,
+                    "Stomach": 0,
+                    "LeftArm": 0,
+                    "RightArm": 0,
+                    "LeftLeg": 0,
+                    "RightLeg": 0
+                };
                 break;
         }
-    
-        this.healths[sessionID] = health;
+
+        this.healths[sessionID] = node;
     }
 
     /* apply the health changes and delete stored ones */
